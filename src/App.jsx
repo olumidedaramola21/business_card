b843ea8740b14f216c9773a9a42ca934a5ff507d
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-08 14:26:20 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Footer from './components/Footer'
+import Header from './components/Header'
 import Main from './components/Main'
 
 function App() {
         return (
                 <div>
-                <p>Hello</p>
+                <Header  />
                 <Main />
                 <Footer />
                 </div>
