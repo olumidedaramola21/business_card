648d3c4ca576392a8f86f2e4444bbee9dcce744c
Olumide <96479799+olumidedaramola21@users.noreply.github.com>
2023-12-08 14:27:04 +0100

    update

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import picture from "../images/picture.png";
+
+export default function Header() {
+    return (
+        <div className="header--container">
+            <img src={picture} alt="" />
+        </div>
+    );
+}
+
